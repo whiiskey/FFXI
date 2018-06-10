4431895d2826eb3aeb129f48bff87461d4d5562b
whiiskey <36735777+whiiskey@users.noreply.github.com>
2018-06-10 18:50:21 -0500

    Update whiiskey-include.lua

--- whiiskey-include.lua
+++ whiiskey-include.lua
@@ -33,6 +33,7 @@ extdata = require('extdata')
 -- Echo is already a windower console command and a ffxi command, so I use ech
 send_command('alias ech input /item "Echo Drops" <me>')
 send_command('alias echodrop input /item "Echo Drops" <me>')
+send_command('alias echodrops input /item "Echo Drops" <me>')
 send_command('alias rem input /item "Remedy" <me>')
 send_command('alias remedy input /item "Remedy" <me>')
 -- Using "hol" seems good, "holy" is the spell, "ho" is Horde Lullaby II, "hw" is healing waltz
@@ -68,7 +69,6 @@ send_command('alias steadfast input /item "Steadfast Tonic" <me>')
 send_command('alias charmb input /item "Charm Buffer" <me>')
 
 
-
 -----------------------------
 --  Gear sets
 -----------------------------
@@ -83,6 +83,8 @@ send_command("alias quick gs equip sets.quick")
 send_command("alias quickb gs equip sets.quickblue")
 send_command("alias cur gs equip sets.midcast.Cure")
 send_command("alias curself gs equip sets.self_healing")
+send_command("alias cursnaset gs equip sets.midcast.Cursna")
+
 
 -- TP
 send_command("alias tp gs equip sets.engaged")
@@ -113,10 +115,169 @@ send_command("alias amalric gs equip sets.amalric")
 send_command("alias th gs equip sets.TreasureHunter")
 send_command("alias enm gs equip sets.enmity")
 send_command("alias enmd gs equip sets.enmitydown")
+send_command("alias frenzy gs equip sets.Frenzy")
 send_command("alias naked gs equip naked")
-  
 
 
+
+-------------------------------------------------------------------------------------------------------------------
+--  MY FUNCTIONS
+--  https://github.com/sethmccauley/GearSwap
+-------------------------------------------------------------------------------------------------------------------
+
+--Arrays for auto-nuking with commands like //gs c nuke blm4 or //gs c nuke blu3
+--Assign //gs c nuke blm4 to a bind key.
+--Then each time you hit the key, you'll nuke the highest priority spell on the blm4 list that's ready for recast.
+--The function chooses the spell based on only the recast timer.  MP has nothing to do with it.
+--If you don't have enough mp, it will try casting the spell anyway, and you'll get "not enough MP" message.
+--The shortcuts lua needs to be running in order for this to work.
+
+
+--This function should probably be it's own addon lua, but idk how to do that
+--It could have a data file with different nuke_arrays.  Kind of like azuresets data file.
+--It would also be good to have the choose_nuke function return an error message if there's no such set.
+--The number next to each spell is it's spell number or something.  I need that to get the recast.
+--I'd like to be able to look up recast or the recast # from the spell's name.  But idk how to do that.
+
+--Add 2 RUN sets: single target hate, AOE hate
+
+nuke_array = {}
+nuke_array.blu3= {
+           {'Spectral Floe',720},
+           {'Tenebral Crush',728},
+           {'Anvil Lightning',721},
+           {'Entomb',722},
+           {'Subduction',708},
+}
+nuke_array.blu2 = {
+           {'Anvil Lightning',721},
+           {'Entomb',722},
+           {'Subduction',708},
+}
+
+
+--These are nukes for Naga Raja.
+--When the dragon flies, melee hits don't work, but nukes do!
+--Naga seems to resist Subduction and Spectral Floe.  But Dark and Light based nukes work.
+--Three of the nukes are AOE spells, which are good for nuking the adds.
+--Make sure Erratic Flutter is up to reduce the recast times.
+--Magic Hammer works well on Naga too.  Cast that if the other spells are down.
+
+--Recast times:
+--Retinal Glare 45 seconds
+--Tenebral Crush 60 seconds
+--Blinding Fulgor 60 seconds
+--Palling Salvo 60 seconds
+--Rail Cannon 80 seconds
+--Magic Hammer 180 seconds
+
+nuke_array.naga= {
+           {'Retinal Glare',707},
+           {'Tenebral Crush',728},
+           {'Blinding Fulgor',725},
+           {'Palling Salvo',724},
+           {'Rail Cannon',712},
+           {'Magic Hammer',646},
+}
+
+
+--On the blm lists, I have AOE spells for Thunder, Blizzard, Fire, and Stone.
+--The mobs I like to nuke a lot resist Water and Aero, so I left those spells off the list.
+--So as of the day I'm writing this, 6/10/2018, its seems like the one's I'll use the most are
+--blm1 for pulling mobs
+--blm4 for nuking them
+--I might not use the others much.  Maaaybe when I'm cleaving weakened?  We'll see.
+nuke_array.blm1 = {
+           {'Thundaga',194},
+           {'Blizzaga',179},
+           {'Firaga',174},
+           {'Stonega',189},
+}
+nuke_array.blm1rev = {
+           {'Stonega',189},
+           {'Firaga',174},
+           {'Blizzaga',179},
+           {'Thundaga',194},
+}
+nuke_array.blm2 = {
+           {'Thundaga II',195},
+           {'Blizzaga II',180},
+           {'Firaga II',175},
+           {'Stonega II',190},
+           {'Thundaga',194},
+           {'Blizzaga',179},
+           {'Firaga',174},
+           {'Stonega',189},
+}
+nuke_array.blm3 = {
+           {'Thundaga III',196},
+           {'Blizzaga III',181},
+           {'Firaga III',176},
+           {'Stonega III',191},
+           {'Thundaga II',195},
+           {'Blizzaga II',180},
+           {'Firaga II',175},
+           {'Stonega II',190},
+           {'Thundaga',194},
+           {'Blizzaga',179},
+           {'Firaga',174},
+           {'Stonega',189},
+}
+nuke_array.blm4 = {
+           {'Thundaja',500},
+           {'Blizzaja',497},
+           {'Firaja',496},
+           {'Stoneja',499},
+           {'Thundaga III',196},
+           {'Blizzaga III',181},
+           {'Firaga III',176},
+           {'Stonega III',191},
+           {'Thundaga II',195},
+           {'Blizzaga II',180},
+           {'Firaga II',175},
+           {'Stonega II',190},
+           {'Thundaga',194},
+           {'Blizzaga',179},
+           {'Firaga',174},
+           {'Stonega',189},
+}
+
+function choose_nuke(array, label)
+        for i,v in pairs(array) do
+            recasttime = windower.ffxi.get_spell_recasts()[v[2]]
+            if recasttime == 0 then
+                send_command(string.gsub(v[1], "%s+", ""))
+                add_to_chat(5,'('..label..' spell	 #'..i..') '..v[1]..' >> '..tostring(player.target.name))
+                return
+            else
+                --add_to_chat(4,v[1]..' recast time: '..recasttime / 100)
+            end
+        end
+        add_to_chat(4,'No spells are ready')
+end
+
+
+--This is needed to pass commands with the //gs c syntax.
+--I guess if a particular gearswap lua has a job_self_command function, it will over-ride this.
+--Most of my luas don't have one, so I don't need to worry about those.
+--But if a lua has this in it, then it needs to add a rule for how to handle the nuke argument.
+function job_self_command(commandArgs, eventArgs)    
+    if commandArgs[1] == 'nuke' then
+        choose_nuke(nuke_array[commandArgs[2]],commandArgs[2])        
+    end    
+    
+    --Generalize this.
+    if commandArgs[1] == 'buffme' then
+        if not buffactive.Haste then
+            send_command('erraticflutter')
+        elseif not buffactive['Aquaveil'] then
+            send_command('aquaveil')
+        elseif not buffactive.Blink then
+            send_command('occultation')
+        end
+    end    
+    
+end
 
 -------------------------------------------------------------------------------------------------------------------
 --  LANGLY FUNCTIONS
@@ -188,6 +349,7 @@ end
 -- I would actually like this fuction to return something, a true/false, a string, something
 
 function checkblocking(spell)
+        --add_to_chat(2,'Spell recast_id: '..spell.recast_id)
 	if buffactive.sleep or buffactive.petrification or buffactive.terror then 
 	   add_to_chat(3,'Canceling Action - Asleep/Petrified/Terror!')
 	   cancel_spell()
@@ -235,6 +397,7 @@ function checkblocking(spell)
 		  end
 	      recasttime = windower.ffxi.get_spell_recasts()[spell.recast_id] / 100
           if spell and (recasttime >= 1) then
+                   
 		   add_to_chat(2,'Spell Canceled:'..spell.name..' - Waiting on Recast:(seconds) '..recasttime..'')
            cancel_spell()
            return
